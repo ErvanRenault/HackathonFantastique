194395e28dc106ede04de28883c601eb13717635
Legendre Matthieu <matthieu1.legendre@laposte.net>
2016-11-29 10:20:31 +0100

    Connection Firebase

--- app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
+++ app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
@@ -2,7 +2,14 @@ package com.mitic.ervan.hackathonfantastique;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("TEST", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TEST", "Failed to read value.", error.toException());
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
