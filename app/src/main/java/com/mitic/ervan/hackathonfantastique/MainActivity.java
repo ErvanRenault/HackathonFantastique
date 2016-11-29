3a61666ec947972699f71c9d1123b3489d45ad6b
Kellam <Florianguenec@hotmail.fr>
2016-11-29 11:10:17 +0100

    Gestion fragments

--- app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
+++ app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
@@ -1,8 +1,13 @@
 package com.mitic.ervan.hackathonfantastique;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.database.DataSnapshot;
@@ -10,8 +15,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mitic.ervan.hackathonfantastique.gestion.GestionEvenement;
+import com.mitic.ervan.hackathonfantastique.parcours.CreerParcours;
+import com.mitic.ervan.hackathonfantastique.parcours.RechercheParcours;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CreerParcours.OnFragmentInteractionListener,GestionEvenement.OnFragmentInteractionListener, RechercheParcours.OnFragmentInteractionListener,Accueil.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +47,53 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("TEST", "Failed to read value.", error.toException());
             }
         });
+
+        Accueil accueil=Accueil.newInstance("param1","param2");
+        FragmentTransaction fragmentManager =  getSupportFragmentManager().beginTransaction();
+        fragmentManager.add(R.id.activity_main,accueil).commit();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.gestevent:
+            case R.id.gestionevenement:
                 gererEvent();
                 return true;
-            case R.id.creerparc:
+            case R.id.creationparcours:
                 createParcours();
                 return true;
-            case R.id.searchpar:
+            case R.id.rechercheparcours:
                 searchParcours();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 
     private void gererEvent(){
-
+        FragmentTransaction fragmentManager =  getSupportFragmentManager().beginTransaction();
+        Fragment gestionEvenement=GestionEvenement.newInstance("param1","param2");
+        fragmentManager.replace(R.id.activity_main,gestionEvenement).addToBackStack(null).commit();
     }
     private void  createParcours(){
-
+        FragmentTransaction fragmentManager =  getSupportFragmentManager().beginTransaction();
+        Fragment creerParcours= CreerParcours.newInstance("param1","param2");
+        fragmentManager.replace(R.id.activity_main,creerParcours).addToBackStack(null).commit();
     }
     private void searchParcours(){
+        FragmentTransaction fragmentManager =  getSupportFragmentManager().beginTransaction();
+        Fragment rechercheParcours= RechercheParcours.newInstance("param1","param2");
+        fragmentManager.replace(R.id.activity_main,rechercheParcours).addToBackStack(null).commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
