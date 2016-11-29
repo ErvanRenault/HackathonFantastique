3d313eaa6681c1271543c3bb6fc9fdb4bde9ce69
Kellam <Florianguenec@hotmail.fr>
2016-11-29 09:59:58 +0100

    menu

--- app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
+++ app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
@@ -1,7 +1,8 @@
 package com.mitic.ervan.hackathonfantastique;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.gestevent:
+                gererEvent();
+                return true;
+            case R.id.creerparc:
+                createParcours();
+                return true;
+            case R.id.searchpar:
+                searchParcours();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void gererEvent(){
+
+    }
+    private void  createParcours(){
+
+    }
+    private void searchParcours(){
+
     }
 }
