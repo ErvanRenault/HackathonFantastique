dc7c949d5ec60e5fd0e6abbad2a0865fab9a0b8c
Legendre Matthieu <matthieu1.legendre@laposte.net>
2016-11-29 17:17:27 +0100

    Structure donnée

---
+++ app/src/main/java/com/mitic/ervan/hackathonfantastique/parcours/Field.java
@@ -0,0 +1,55 @@
+package com.mitic.ervan.hackathonfantastique.parcours;
+
+/**
+ * Created by ce on 29/11/16.
+ */
+public class Field {
+    public String acces_itineraire_fr;
+    public String adresse;
+    public String animateurs;
+    public String apercu;
+    public String arrondissement;
+    public String code_postal;
+    public String conditions_d_acces;
+    public String date_debut;
+    public String date_debut_jour;
+    public String date_debut_mois;
+    public String date_fin;
+    public String date_fin_jour;
+    public String date_fin_mois;
+    public String dates;
+    public String departement;
+    public String derniere_mise_a_jour;
+    public String description_du_lieu_fr;
+    public String description_fr;
+    public String description_longue_fr;
+    public String description_longue_html_fr;
+    public String detai_des_conditions_fr;
+    public float geolocalisation[];
+    public String horaires_detailles_fr;
+    public String horaires_iso;
+    public String identifiant;
+    public String identifiant_du_lieu;
+    public String image;
+    public String image_source;
+    public String lien;
+    public String lien_d_inscription;
+    public String liens_du_lieu;
+    public String mots_cles_fr;
+    public String nom_du_lieu;
+    public String pays;
+    public String publics_concernes;
+    public String region;
+    public String resume_dates_fr;
+    public String resume_horaires_fr;
+    public String selection;
+    public String site_web_du_lieu;
+    public String statut;
+    public String telephone_du_lieu;
+    public String thematiques;
+    public String titre_fr;
+    public String type_d_animation;
+    public String types_de_public;
+    public String ville;
+
+}
