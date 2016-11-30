303a807d0f352044988303300cd2abdecbe8ec51
Legendre Matthieu <matthieu1.legendre@laposte.net>
2016-11-30 09:36:07 +0100

    Info Liste + detail Evenement

--- app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
+++ app/src/main/java/com/mitic/ervan/hackathonfantastique/MainActivity.java
@@ -22,7 +22,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.mitic.ervan.hackathonfantastique.dummy.DummyContent;
 import com.mitic.ervan.hackathonfantastique.data.Data;
 import com.mitic.ervan.hackathonfantastique.data.Evenement;
 import com.mitic.ervan.hackathonfantastique.data.EvenementFactory;
@@ -30,7 +29,6 @@ import com.mitic.ervan.hackathonfantastique.event.Event;
 import com.mitic.ervan.hackathonfantastique.event.ListEvent;
 import com.mitic.ervan.hackathonfantastique.gestion.GestionEvenement;
 import com.mitic.ervan.hackathonfantastique.map.MapRechercheEvent;
-import com.mitic.ervan.hackathonfantastique.map.MyMapFragment;
 import com.mitic.ervan.hackathonfantastique.parcours.CreerParcours;
 import com.mitic.ervan.hackathonfantastique.parcours.RechercheParcours;
 
@@ -162,34 +160,6 @@ public class MainActivity extends AppCompatActivity implements CreerParcours.OnF
         fragmentManager.replace(R.id.activity_main,listEvent).addToBackStack(null).commit();
     }
 
-    public void ajouterEvenement(View view) {
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.elementsparcours);
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerparcours);
-
-        final ViewGroup linearLayout = new LinearLayout(MainActivity.this);
-        TextView nomEvent = new TextView(MainActivity.this);
-        final Button erase = new Button(MainActivity.this);
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        LinearLayout.LayoutParams paramsElem = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1.0f);
-
-        linearLayout.setLayoutParams(params);
-        erase.setLayoutParams(paramsElem);
-        erase.setText("retirer");
-        erase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((ViewGroup) linearLayout.getParent()).removeView(linearLayout);
-                Log.d("TAG", "onClick: erase");
-            }
-        });
-        nomEvent.setLayoutParams(paramsElem);
-        nomEvent.setText(spinner.getSelectedItem().toString());
-        linearLayout.addView(nomEvent);
-        linearLayout.addView(erase);
-        layout.addView(linearLayout);
-    }
     @Override
     public void onFragmentInteraction(Uri uri) {
 
